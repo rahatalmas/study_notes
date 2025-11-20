d942005356c8864aa285939707de72c79164546a
rahatalmas <rahat.almas5599@gmail.com>
2025-11-20 18:46:38 +0600

    middleware_added

--- spider/src/app.module.ts
+++ spider/src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { BlogsModule } from './modules/blogs/blogs.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [UsersModule, BlogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  configure(consumer: MiddlewareConsumer){
+     consumer.apply(LoggerMiddleware).forRoutes("*")
+  }
+}
